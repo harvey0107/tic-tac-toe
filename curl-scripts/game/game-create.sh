2c00cdb1c3fb21fd4710bef07bf7aab2089e44d4
rachel yang <rachel@rachels-MacBook-Air.local>
2020-08-30 17:14:29 -0400

    8/30 committed

---
+++ curl-scripts/game/game-create.sh
@@ -0,0 +1,14 @@
+# VARIABLE=VALUE sh curl-scripts/auth/sign-in.sh
+
+curl "https://library-express-api.herokuapp.com/sign-in" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
